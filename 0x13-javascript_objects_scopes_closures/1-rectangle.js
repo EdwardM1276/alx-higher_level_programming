d12e4ddca345c00ed35fb9b4b25684a22b479d9d
EdwardM1276 <letebeleM98@gmail.com>
2023-03-07 18:17:04 -0800

    Commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,59 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"1-rectangle.js" [New File]                                                                                                                                                   0,0-1         All
+
